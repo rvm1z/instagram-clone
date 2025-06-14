815bb0798e9189f20a1717e459b7ed592e3b9521
Ramiz Raza <rr2856@ece.jgec.ac.in>
2025-06-14 10:17:54 +0530

    Add User model for authentication and social features

---
+++ server/models/user.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  profilePic: {
+    type: String,
+    default: ''
+  },
+  followers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  following: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
+}, { timestamps: true });
+
+module.exports = mongoose.model('User', UserSchema);
