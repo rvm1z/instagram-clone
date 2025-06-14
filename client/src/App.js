cfa097c42011c650e0ce6a169a6d65548827d69f
Ramiz Raza <rr2856@ece.jgec.ac.in>
2025-06-14 10:48:17 +0530

    Integrate Navbar component and wrap Feed/CreatePost routes with PrivateRoute for authentication

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import Navbar from './components/Navbar';
 import CreatePost from './pages/CreatePost';
+import PrivateRoute from './components/PrivateRoute';
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
@@ -9,16 +10,29 @@ import Feed from './pages/Feed';
 
 function App() {
   return (
-    <Router>
-    <Navbar />
-      <Routes>
-        <Route path="/" element={<Feed />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-       <Route path="/create-post" element={<CreatePost />} />
-
-      </Routes>
-    </Router>
+   <Router>
+  <Navbar />
+  <Routes>
+    <Route
+      path="/"
+      element={
+        <PrivateRoute>
+          <Feed />
+        </PrivateRoute>
+      }
+    />
+    <Route path="/login" element={<Login />} />
+    <Route path="/register" element={<Register />} />
+    <Route
+      path="/create-post"
+      element={
+        <PrivateRoute>
+          <CreatePost />
+        </PrivateRoute>
+      }
+    />
+  </Routes>
+</Router>
   );
 }
 
