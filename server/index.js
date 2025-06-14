707110e37b084934b9be4d3a91c0391d06d28df9
Ramiz Raza <rr2856@ece.jgec.ac.in>
2025-06-14 10:52:29 +0530

    Serve uploads folder statically in server/index.js for image access

--- server/index.js
+++ server/index.js
@@ -15,6 +15,7 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // Routes
 app.use('/api/users', userRoutes);
