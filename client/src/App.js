a4185cf3eaced79c5b49f74c9be58af2d6e9c957
Ramiz Raza <rr2856@ece.jgec.ac.in>
2025-06-14 10:29:29 +0530

    Set up React Router with Login, Register, and Feed routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Feed from './pages/Feed';
 
 function App() {
   return (
-    <div>
-      <h1>Welcome to Instagram Clone</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Feed />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
   );
 }
 
